ab4ca89b16d68b0b35064de0a5e04aa2a3da9e28
wrcj12138aaa <wrcj12138@163.com>
2019-05-04 11:53:39 +0800

    - 项目初始化

---
+++ src/main/java/com/one/learn/spring/officedoc/OfficedocApplication.java
@@ -0,0 +1,13 @@
+package com.one.learn.spring.officedoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficedocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficedocApplication.class, args);
+    }
+
+}
